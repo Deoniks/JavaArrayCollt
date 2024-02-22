d7caf252005bd73a991a3af43588bdf3ec34b6d3
Deoniks <dmitrykv97@gmail.com>
2024-02-22 19:30:14 +0600

    upd two question

--- CollectionLesson.java
+++ CollectionLesson.java
@@ -1,4 +1,32 @@
 package lessColl;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class CollectionLesson {
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        /*for(int i =0; i<oneQuesion().size();i++){
+            System.out.println(oneQuesion().get(i));
+        } firs prog*/
+
+        twoQuestion();
+    }
+
+    public static ArrayList<String> oneQuesion(){
+        ArrayList<String> color = new ArrayList<>();
+        color.add(0,"Red"); //
+        color.add(1,"Green");
+        color.add(2,"Blue");
+
+        return color;
+    }
+
+    public static void twoQuestion(){
+        Iterator<String> iterationColor = oneQuesion().listIterator();
+        while (iterationColor.hasNext()){
+            System.out.println(iterationColor.next());
+        }
+    }
 }
